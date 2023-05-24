906b63df001c206e8e712f7edcf4fc5af0e0e2f8
Kira Burova <burova@talon.one>
2023-05-24 11:06:36 +0200

    added functions to list and add timezones

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
+var Timezones []string
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/timezones", listTimezones)
+	myRouter.HandleFunc("/timezone", addTimezone).Methods("POST")
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
 func main() {
-	timezone := getCurrentTimezone()
-	fmt.Println(timezone)
+	Timezones = []string{"Europe/Berlin", "Africa/Abidjan", "Africa/Addis_Ababa"}
+
+	handleRequests()
 }
 
 func getCurrentTimezone() string {
@@ -17,12 +32,19 @@ func getCurrentTimezone() string {
 	return zone
 }
 
-func addTimezone() {
-	// choose a timezone to add somehow ?? from a static list from somewhere, for example
-	// add to array -> save somewhere -> database ??
+func addTimezone(w http.ResponseWriter, r *http.Request) {
+	// read posted timezone
+	timezone, _ := ioutil.ReadAll(r.Body)
+
+	// set new timezone to timezones array
+	Timezones = append(Timezones, string(timezone))
+
+	json.NewEncoder(w).Encode(timezone)
 }
-func listTimezones() {
-	// get list of timezones from database
+func listTimezones(w http.ResponseWriter, r *http.Request) {
+	// get list of timezones in json format
+	json.NewEncoder(w).Encode(Timezones)
+
 }
 func convertTimezone() {
 	// get timezone getCurrentTimezone()
