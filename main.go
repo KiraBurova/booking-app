e6b0bf969a3800adf3bb32389899304a7372fa92
Kira Burova <burova@talon.one>
2023-06-27 12:35:03 +0200

    added users db and register user endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,6 +10,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var Timezones []string
@@ -18,15 +20,57 @@ func handleRequests() {
 	myRouter.HandleFunc("/timezones", listTimezones)
 	myRouter.HandleFunc("/timezone", addTimezone).Methods("POST")
 	myRouter.HandleFunc("/convert_timezone", convertTimezone).Methods("POST")
+
+	myRouter.HandleFunc("/register", register).Methods("POST")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
 	Timezones = []string{"Europe/Berlin", "Africa/Abidjan", "Africa/Addis_Ababa"}
 
+	createDb()
 	handleRequests()
 }
 
+/* USER */
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+const create string = `create table users(username text, password text);`
+
+func createDb() error {
+	db, err := sql.Open("sqlite3", "./users.db")
+	if err != nil {
+		return err
+	}
+	if _, err := db.Exec(create); err != nil {
+		return err
+	}
+	return nil
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	// get posted data for user and store into db
+	var u User
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	json.NewEncoder(w).Encode(u)
+
+	db, _ := sql.Open("sqlite3", "./users.db")
+
+	stmt, _ := db.Prepare("INSERT INTO users(username, password) values(?,?)")
+
+	res, _ := stmt.Exec(u.Username, u.Password)
+
+	id, _ := res.LastInsertId()
+
+	fmt.Println(id)
+}
+
+/* TIMEZONES */
 func addTimezone(w http.ResponseWriter, r *http.Request) {
 	// read posted timezone
 	timezone, _ := ioutil.ReadAll(r.Body)
