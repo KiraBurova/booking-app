2b2f03d4e41bda4d8cdd8f274513bb4485db6d92
Kira Burova <burova@talon.one>
2023-07-06 09:11:30 +0200

    formatting

--- main.go
+++ main.go
@@ -20,7 +20,6 @@ func handleRequests() {
 	myRouter.HandleFunc("/timezones", listTimezones)
 	myRouter.HandleFunc("/timezone", addTimezone).Methods("POST")
 	myRouter.HandleFunc("/convert_timezone", convertTimezone).Methods("POST")
-
 	myRouter.HandleFunc("/register", register).Methods("POST")
 	myRouter.HandleFunc("/book_time/{userId}", bookTime).Methods("POST")
 
@@ -62,7 +61,6 @@ func createDb() error {
 func register(w http.ResponseWriter, r *http.Request) {
 	// get posted data for user and store into db
 	var u User
-
 	defaultTimeslots := map[string]TimeslotStatus{
 		"9:00":  {Booked: false},
 		"10:00": {Booked: false},
@@ -75,51 +73,35 @@ func register(w http.ResponseWriter, r *http.Request) {
 	}
 
 	u.Timeslots = defaultTimeslots
-
 	jsonWithTimeslots, _ := json.Marshal(defaultTimeslots)
-
 	json.NewDecoder(r.Body).Decode(&u)
-
 	db, _ := sql.Open("sqlite3", "./users.db")
-
 	stmt, _ := db.Prepare("INSERT INTO users(username, password, timeslots) values(?,?,?)")
-
 	res, _ := stmt.Exec(u.Username, u.Password, jsonWithTimeslots)
-
 	id, _ := res.LastInsertId()
 
 	fmt.Println(id)
-
 	json.NewEncoder(w).Encode(u)
 }
 
 func bookTime(w http.ResponseWriter, r *http.Request) {
-
 	params := mux.Vars(r)
-
 	userId := params["userId"]
-
 	db, _ := sql.Open("sqlite3", "./users.db")
-
 	row := db.QueryRow("SELECT * FROM users WHERE id=?", userId)
-
 	user := User{}
-
 	row.Scan(&user.Id, &user.Username, &user.Password, &user.Timeslots)
 
 	// TODO: Timeslots seems to be empty there, figure out why
 	fmt.Println(user.Timeslots)
-
 }
 
 /* TIMEZONES */
 func addTimezone(w http.ResponseWriter, r *http.Request) {
 	// read posted timezone
 	timezone, _ := ioutil.ReadAll(r.Body)
-
 	// set new timezone to timezones array
 	Timezones = append(Timezones, string(timezone))
-
 	json.NewEncoder(w).Encode(timezone)
 }
 func listTimezones(w http.ResponseWriter, r *http.Request) {
@@ -129,10 +111,7 @@ func listTimezones(w http.ResponseWriter, r *http.Request) {
 }
 func convertTimezone(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
-
 	timezone, _ := ioutil.ReadAll(r.Body)
-
 	loc, _ := time.LoadLocation(string(timezone))
-
 	fmt.Printf("Time in %v: %s\n", string(timezone), now.In(loc))
 }
